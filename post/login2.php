c4d84a746df03fb185ba614d64e93d2eb56fb9b1
vtrpaulop <119384161+vtrpaulop@users.noreply.github.com>
2023-08-24 01:31:51 -0300

    alterações no front

--- post/login2.php
+++ post/login2.php
@@ -15,24 +15,29 @@ if (isset($_SESSION['login_error'])) {
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="\css\reset.css">    
     <link rel="stylesheet" href="\css\index.css">
+    <link rel="stylesheet" href="\css\login2.css">
     <link rel="stylesheet" href="\css\responsiva.css">
     <title>Login</title>
 </head>
 <body>
     <div id="header">
-    <img class="logo" src="..\img\logo.png">
+    <img class="logo" src="..\img\logo3.png">
     <p class="titletop"> ESTE É O CABEÇALHO </p>
-    <h1>Login</h1>
+    <div id="login">
+    <h1>Login</h1><br>
     <form action="autenticado.php" method="post">
         <label for="username">Usuário:</label>
-        <input type="text" name="username" required><br>
+        <input type="text" name="username" required><br><br>
 
-        <label for="password">Senha:</label>
-        <input type="password" name="password" required><br>
+        <label for="password">Senha:   </label>
+        <input type="password" name="password" required><br><br>
 
         <input type="submit" value="Entrar">
+        <button type="button" name="voltar"><a href="novidades.php">Voltar</button></a>
+        
     </form>
-    <button><a href="novidades.php">Voltar</a></button>
+</div>
+    
     <div id="footer"><p id="copy">&copy; Projeto PI II  </p></div>
 </body>
 </html>
