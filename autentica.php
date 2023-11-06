da2d5da7132ace4a911fafc7d742b1026e5a23c0
vtrpaulop <119384161+vtrpaulop@users.noreply.github.com>
2023-11-06 12:55:34 -0300

    alteração css

--- autentica.php
+++ autentica.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost:3306";
-$usuario_bd = "root";
-$senha_bd = "";
-$banco = "projetopi";
+$host = "wpmysql0004.locaweb.com.br";
+$usuario_bd = "wp_1029984403";
+$senha_bd = "bd57b27549";
+$banco = "wp_1029984403";
 
 
 $conn = new mysqli($host, $usuario_bd, $senha_bd, $banco);
