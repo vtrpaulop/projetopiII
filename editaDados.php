7172b11ac22127b77ee3b215cc9c246c8dce0e39
vtrpaulop <119384161+vtrpaulop@users.noreply.github.com>
2023-11-06 12:56:39 -0300

    alterando a edição dados

---
+++ editaDados.php
@@ -0,0 +1,89 @@
+<?php
+require_once 'autentica.php';
+
+// Verifica se o ID do usuário foi fornecido na sessão
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: loginU.php");
+    exit();
+}
+
+$escapedId = $_SESSION['user_id'];
+
+// Busca as informações do usuário pelo ID
+$query = "SELECT * FROM users WHERE id = $escapedId";
+$result = $conn->query($query);
+
+// Verifica se a consulta foi bem-sucedida
+if ($result !== false && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $username = $row['username'];
+    $password = $row['password'];
+    $nome = $row['nome'];
+    $sobrenome = $row['sobrenome'];
+    $dnascimento = $row['dnascimento'];
+    $telefone = $row['telefone'];
+    $email = $row['email'];
+    $sexo = $row['sexo'];
+} else {
+    echo "<p class='error'>Usuário não encontrado.</p>";
+    exit();
+}
+
+// Processamento do formulário de edição
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $password = $_POST["password"];
+    $nome = $_POST["nome"];
+    $sobrenome = $_POST["sobrenome"];
+    $dnascimento = $_POST["dnascimento"];
+    $telefone = $_POST["telefone"];
+    $email = $_POST["email"];
+    $sexo = $_POST["sexo"];
+
+    // Atualiza as informações do usuário no banco de dados
+    $query = "UPDATE users SET password='$password', nome='$nome', sobrenome='$sobrenome', dnascimento='$dnascimento', telefone='$telefone', email='$email', sexo='$sexo' WHERE id=$escapedId";
+    if ($conn->query($query) === TRUE) {
+        echo "<p>Informações atualizadas com sucesso.</p>";
+    } else {
+        echo "<p class='error'>Erro ao atualizar informações: " . $conn->error . "</p>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="..\css\reset.css">
+    <link rel="stylesheet" href="..\css\cadastro.css">
+    <link rel="stylesheet" href="..\css\responsiva.css">
+    <title>Edição de Usuário</title>
+</head>
+<body>
+<h2>Edição de Usuário</h2>
+<form id="userForm" method="post" action="">
+    <label for="username">Usuário:</label>
+    <input type="text" id="cad" name="username" value="<?php echo $username; ?>" readonly>
+    <label for="password">Password:</label>
+    <input type="text" id="cad" name="password" value="<?php echo $password; ?>" required>
+    <label for="nome">Nome:</label>
+    <input type="text" id="cad" name="nome" value="<?php echo $nome; ?>" autofocus required>
+    <label for="sobrenome">Sobrenome:</label>
+    <input type="text" id="cad" name="sobrenome" value="<?php echo $sobrenome; ?>" required>
+    <label for="dnascimento">Idade:</label>
+    <input type="date" id="cad" name="dnascimento" value="<?php echo $dnascimento; ?>" required>
+    <label for="telefone">Telefone:</label>
+    <input type="text" id="cad" name="telefone" value="<?php echo $telefone; ?>" required>
+    <label for="email">Email:</label>
+    <input type="text" id="cad" name="email" value="<?php echo $email; ?>" required>
+    <label for="sexo">Escolha seu sexo:</label><br>
+    <input type="radio" name="sexo" value="masculino" <?php if ($sexo == "masculino") echo "checked"; ?>>Masculino<br>
+    <input type="radio" name="sexo" value="feminino" <?php if ($sexo == "feminino") echo "checked"; ?>>Feminino<br>
+    <input type="radio" name="sexo" value="outro" <?php if ($sexo == "outro") echo "checked"; ?>>Outro<br><br>
+    <input type="submit" value="Salvar"><br>
+</form>
+<div id="botoes">
+    <a href="painelUser.php"><input id="voltar" type="submit" name="voltar" value="Voltar"></a>
+</div>
+</body>
+</html>
